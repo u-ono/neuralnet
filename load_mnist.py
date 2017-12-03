822eef623dfbb97c564a6074dc138be92b5912df
u-ono <oono.yuu@gmail.com>
2017-12-03 18:38:25 +0900

    Add load_mnist.py

---
+++ load_mnist.py
@@ -0,0 +1,34 @@
+import pickle
+import numpy as np
+
+train_num = 60000
+test_num = 10000
+img_dim = (1, 28, 28)
+img_size = 784
+
+def load_mnist(normalize=True, flatten=True, one_hot_label=False):
+        
+    with open('mnist.pkl', 'rb') as f:
+        dataset = pickle.load(f)
+    
+    if normalize:
+        for key in ('train_img', 'test_img'):
+            dataset[key] = dataset[key].astype(np.float32)
+            dataset[key] /= 255.0
+            
+    if one_hot_label:
+        dataset['train_label'] = _change_one_hot_label(dataset['train_label'])
+        dataset['test_label'] = _change_one_hot_label(dataset['test_label'])
+    
+    if not flatten:
+         for key in ('train_img', 'test_img'):
+            dataset[key] = dataset[key].reshape(-1, 1, 28, 28)
+
+    return (dataset['train_img'], dataset['train_label']), (dataset['test_img'], dataset['test_label']) 
+
+def _change_one_hot_label(X):
+    T = np.zeros((X.size, 10))
+    for idx, row in enumerate(T):
+        row[X[idx]] = 1
+        
+    return T
--- train_net.py
+++ train_net.py
@@ -1,9 +1,9 @@
-import sys, os
-sys.path.append(os.pardir)
+#import sys, os
+#sys.path.append(os.pardir)
 
 import pickle
+from load_mnist import *
 import numpy as np
-from dataset.mnist import load_mnist
 from neuralnet import NeuralNet
 
 # read the dataset
